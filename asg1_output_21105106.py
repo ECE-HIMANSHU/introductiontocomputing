c026c5025b4ebefde13d006f6095c078f9f7cd8c
ECE-HIMANSHU <97097665+ECE-HIMANSHU@users.noreply.github.com>
2022-01-05 12:45:43 +0530

    Add files via upload

---
+++ asg1_output_21105106.py
@@ -0,0 +1,70 @@
+# OUTPUT OF QUESTION 1
+"""
+C:\Users\himan\PycharmProjects\pythonProject\venv\Scripts\python.exe "D:/ASSIGNMENT COMPUTNG/asg1_21105106.py"
+WRITE YOUR NUMBERS BELOW.
+TYPE NUMBER 1
+8
+TYPE NUMBER 2
+9
+TYPE NUMBER 3
+10
+THE AVERAGE OF YOUR NUMBERS IS 9.0
+
+Process finished with exit code 0
+"""
+# OUTPUT OF QUESTION 2
+"""
+C:\Users\himan\PycharmProjects\pythonProject\venv\Scripts\python.exe "D:/ASSIGNMENT COMPUTNG/asg1_21105106.py"
+INFORMATION USED IS 
+ TAX RATE=20%,STANDARD DEDUCTION=$10000,DEPENDENT DEDUCTION=$3000 PER DEPENDENT
+ENTER YOUR GROSS INCOME TO IN "$"(TO NEAREST PENNY).
+50000
+ENTER YOU NUMBER OF DEPENDENTS.
+10
+YOUR INCOME TAX IS $ 2000.0
+
+Process finished with exit code 0
+"""
+# OUTPUT OF QUESTION 3
+"""
+C:\Users\himan\PycharmProjects\pythonProject\venv\Scripts\python.exe "D:/ASSIGNMENT COMPUTNG/asg1_21105106.py"
+LIST IS IN THE FORMAT OF [STUDENT ID,NAME,GENDER,COURSE NAME,CGPA]
+FOR GENDER (F-FEMALE),(M-MALE),(U-UNKOWN) 
+MY DETAILS IN MY LIST ARE [21105106, 'HIMANSHU', 'M', 'ECE', 9.1]
+
+Process finished with exit code 0
+"""
+# OUTPUT OF QUESTION 4
+"""
+C:\Users\himan\PycharmProjects\pythonProject\venv\Scripts\python.exe "D:/ASSIGNMENT COMPUTNG/asg1_21105106.py"
+ENTER THE MARKS OF STUDENTS B/W 1 TO 100
+ENTER MARKS OF STUDENT 1:56
+ENTER MARKS OF STUDENT 2:70
+ENTER MARKS OF STUDENT 3:23
+ENTER MARKS OF STUDENT 4:90
+ENTER MARKS OF STUDENT 5:44
+THE SORTED MARKS OF STUDENTS FROM INCREASING TO DECREASING ORDER IN A LIST ARE: 
+ [23, 44, 56, 70, 90]
+
+Process finished with exit code 0
+"""
+# OUTPUT OF QUESTION 5(a)
+"""
+C:\Users\himan\PycharmProjects\pythonProject\venv\Scripts\python.exe "D:/ASSIGNMENT COMPUTNG/asg1_21105106.py"
+THE LIST GIVEN IS 
+ ['RED', 'GREEN', 'WHITE', 'BLACK', 'PINK', 'YELLOW']
+THE UPDATED LIST IS AFTER REMOVING 4TH ELEMENT IS 
+ ['RED', 'GREEN', 'WHITE', 'PINK', 'YELLOW']
+
+Process finished with exit code 0
+"""
+# OUTPUT OF QUESTION 5(b)
+"""
+C:\Users\himan\PycharmProjects\pythonProject\venv\Scripts\python.exe "D:/ASSIGNMENT COMPUTNG/asg1_21105106.py"
+THE LIST GIVEN IS 
+ ['RED', 'GREEN', 'WHITE', 'BLACK', 'PINK', 'YELLOW']
+THE UPDATED LIST IS AFTER REPLACING BLACK,PINK WITH PURPLE IS 
+ ['RED', 'GREEN', 'WHITE', 'PURPLE', 'YELLOW']
+
+Process finished with exit code 0
+"""
