f355259066000fb6fcdf878b2fa803826028825f
ECE-HIMANSHU <97097665+ECE-HIMANSHU@users.noreply.github.com>
2022-01-18 11:04:42 +0530

    Add files via upload

---
+++ asg2_output_21105106.py
@@ -0,0 +1,78 @@
+# OUTPUT 1
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/asg2_21105106.py
+THE GIVEN STRING IS: 'Python is a case sensitive language'
+(a)- THE LENGTH OF THE STRING IS 35
+(b)- STRING AFTER REVERSING ITS ORDER IS-: egaugnal evitisnes esac a si nohtyP
+(c)- A NEW STRING CONTAINING A PART OF GIVEN STRING AS:- case sensitive
+(d)- FOLLOWING GIVEN INSTRUCTIONS NEW STRING WILL BE:-  Python is object oriented language
+(e)- THE INDEX OF SUBSTRING 'a' IS 10
+(f)- REMOVING BLANK SPACES FROM STRING :- Pythonisacasesensitivelanguage
+
+Process finished with exit code 0
+"""
+# OUTPUT 2
+"""
+\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/asg2_21105106.py
+PLEASE ENTER YOUR NAME:
+HIMANSHU
+PLEASE ENTER YOUR SID:
+21105106
+PLEASE WRITE YOUR RESPECTIVE DEPARTMENT:
+ECE
+ENTER YOUR CGPA:
+9.9
+THE OUTPUT IS:
+Hey,HIMANSHU Here!
+My SID is 21105106
+I am from ECE department and my CGPA is 9.9
+
+Process finished with exit code 0
+"""
+# OUTPUT 3
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/asg2_21105106.py
+(a):- RESULT AFTER USING (&) B/W a AND b is: 8
+(b):- RESULT AFTER USING (|) B/W a AND b is: 58
+(c):- RESULT AFTER USING (^) B/W a AND b is: 50
+(d):- LEFT SHIFTING BOTH a AND b BY 2 bit will be: a- 224 b- 40
+(e):- RIGHT SHIFTING a WITH 2 bits AND b WITH b WITH 4 bits will be: a- 14 b- 0
+
+Process finished with exit code 0
+"""
+# OUTPUT 4
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/asg2_21105106.py
+ENTER NUMBER 1:54
+ENTER NUMBER 2:24
+ENTER NUMBER 3:7
+ MAXIMUM OF THREE NUMBERS ENTERED BY YOU IS:
+ 54
+
+Process finished with exit code 0
+"""
+# OUTPUT 5
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/asg2_21105106.py
+IF 'name' IS PRESENT IN YOUR STRING OUTPUT WILL BE YES OTHERWISE NO.
+PLSS WRITE IN SMALL LETTERS
+KINDLY WRITE THE LINE IN WHICH PRESENCE OF "name" TO BE FOUND
+i am himanshu of ece branch and my friend name is ravinoor
+YESS, ITS PRESENT
+
+Process finished with exit code 0
+"""
+# output 6
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/asg2_21105106.py
+BY YOUR THREE NUMBERS IF TRIANGLE COULD BE FORMED THEN OUTPUT WILL BE YESS ELSE NO.
+ENTER FIRST INTEGER 
+3
+ENTER SECOND INTEGER 
+4
+ENTER THIRD INTEGER 
+5
+YES, TRIANGLE COULD BE FORMED
+
+Process finished with exit code 0
+"""
