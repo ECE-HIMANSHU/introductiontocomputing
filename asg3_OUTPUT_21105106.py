49839a801036b0085a54163d191dab76625d946f
ECE-HIMANSHU <97097665+ECE-HIMANSHU@users.noreply.github.com>
2022-02-14 12:30:38 +0530

    Add files via upload

---
+++ asg3_OUTPUT_21105106.py
@@ -0,0 +1,130 @@
+# OUTPUT 1
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/asg3_OUTPUT_21105106.py
+PLEASE TYPE YOUR STRING :
+Himanshu
+LETTER COUNT OF YOUR WORD IS
+ {'h': 2, 'i': 1, 'm': 1, 'a': 1, 'n': 1, 's': 1, 'u': 1}
+
+Process finished with exit code 0
+"""
+
+# OUTPUT 2
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/asg3_OUTPUT_21105106.py
+TYPE DATE [1-31] : 29
+TYPE MONTH [1-12] : 2
+TYPE YEAR [1800-2025] : 2000
+NEXT DATE OF THE INPUT DATE IS :
+ 1 / 3 / 2000
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 3
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/asg3_OUTPUT_21105106.py
+ENTER THE TOTAL (NUMBERS/TUPLES) YOU WANT IN LIST :
+3
+ENTER NUMBER  1  :
+5
+ENTER NUMBER  2  :
+7
+ENTER NUMBER  3  :
+2.8
+USER LIST IS : [5.0, 7.0, 2.8]
+FINAL LIST IS :
+ [(5.0, 25.0), (7.0, 49.0), (2.8, 7.839999999999999)]
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 4
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/asg3_OUTPUT_21105106.py
+PLEASE TYPE YOUR GRADE POINTS :
+6
+YOUR GRADE IS 'C+' AND PERFORMANCE IS AVERAGE.
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 5
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/asg3_OUTPUT_21105106.py
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 6
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/asg3_OUTPUT_21105106.py
+ENTER S_ID OF STUDENT : 21105106
+ENTER NAME OF STUDENT : HIMANSHU
+DO YOU WANT TO ENTER ANOTHER STUDENT ENTRY (Y OR N) : Y
+ENTER S_ID OF STUDENT : 21104081
+ENTER NAME OF STUDENT : RAVINOOR
+DO YOU WANT TO ENTER ANOTHER STUDENT ENTRY (Y OR N) : Y
+ENTER S_ID OF STUDENT : 21103101
+ENTER NAME OF STUDENT : PRRANAV
+DO YOU WANT TO ENTER ANOTHER STUDENT ENTRY (Y OR N) : N
+DICTIONARY ACCORDING TO INPUT : 
+A-> {21105106: 'HIMANSHU', 21104081: 'RAVINOOR', 21103101: 'PRRANAV'}
+DICTIONARY WITH SORTED NAMES : 
+B-> {21105106: 'HIMANSHU', 21103101: 'PRRANAV', 21104081: 'RAVINOOR'}
+DICTIONARY WITH SORTED S_ID : 
+C-> {21103101: 'PRRANAV', 21104081: 'RAVINOOR', 21105106: 'HIMANSHU'}
+SEARCH WITH S_ID
+SEARCH NAME THROUGH SID : 
+21104081
+D-> RAVINOOR
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 7
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/asg3_OUTPUT_21105106.py
+TYPE NUMBER OF TERMS IN THE SERIES : 11
+FIBONACCI SEQUENCE :
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+ AVEREAGE OF RESULTANT FIBONACCI SERIES IS :
+ 13.0
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 8
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/asg3_OUTPUT_21105106.py
+A=>  {1, 3, 5, 6, 8}
+B=>  {3, 6, 8, 9, 13, 17}
+C=>  {1, 2, 4, 5}
+D=>  {6, 7, 8, 9, 10}
+E=>  {10, 7}
+
+Process finished with exit code 0
+
+"""
