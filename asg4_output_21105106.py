a110b40013f50ca117412b131bb05b39e1b8a1c3
ECE-HIMANSHU <97097665+ECE-HIMANSHU@users.noreply.github.com>
2022-03-01 16:37:31 +0530

    Add files via upload

---
+++ asg4_output_21105106.py
@@ -0,0 +1,101 @@
+# OUTPUT 1
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/venv/asg4_outputrun_21105106.py
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+Process finished with exit code 0
+"""
+
+# OUPUT 2
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/venv/asg4_outputrun_21105106.py
+PLEASE ENTER NUMBER OF ROWS IN PASCAL'S TRIANGLE : 
+6
+
+ USING RECURSIONS : 
+
+     1  
+    1  1  
+   1  2  1  
+  1  3  3  1  
+ 1  4  6  4  1  
+1  5  10  10  5  1  
+
+ USING LOOPS : 
+
+     1  
+    1  1  
+   1  2  1  
+  1  3  3  1  
+ 1  4  6  4  1  
+1  5  10  10  5  1  
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 3
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/venv/asg4_outputrun_21105106.py
+PLEASE ENTER FIRST INTEGER : 4
+PLESE ENTER SECOND INTEGER : 2
+THE REMAINDER IS:  0
+THE QUOTIENT IS:  2
+ ONE OF THE VALUES IS ZERO
+{6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+IMMUTABLE SET :  frozenset({8, 5, 6, 7})
+LARGEST VALUE IN THE SET:  8
+HASH VALUE:  8
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 4
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/venv/asg4_outputrun_21105106.py
+OBJECT CREATED.
+THE NAME OF THE STUDENT IT HIMANSHU AND S_ID IS 21105106.
+OBJECT DESTROYED..
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 5
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/venv/asg4_outputrun_21105106.py
+A) THE UPDATED SALARY OF THE EMPLOYEE MEHAK IS 70000
+B) EMPLOYEE VIREN RECORD DELETED.
+
+Process finished with exit code 0
+
+"""
+
+# OUTPUT 6
+"""
+C:\Users\himan\AppData\Local\Programs\Python\Python310\python.exe C:/Users/himan/PycharmProjects/pythonProject/venv/asg4_outputrun_21105106.py
+ENTER THE FIRST WORD :
+MAKE
+
+ ENTER A MEANINGFUL WORD FROM LETTERS OF FIRST :
+AKME
+
+DOES THE WORD HAVE MEANING ?(Y OR N), USE UPPER CASE LETTERS
+N
+THE WORD DON'T HAVE A MEANING, FRIENDSHIP IS FAKE...
+
+Process finished with exit code 0
+
+"""
